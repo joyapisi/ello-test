47ca8fecdb79f005b84e916d451b61432387fb60
Joy Phoebe <joyapisi@gmail.com>
2024-07-01 18:43:40 +0300

    begint to populate reading list

--- frontend/src/components/readingList.js
+++ frontend/src/components/readingList.js
@@ -1,6 +1,15 @@
+import React from "react";
+import { List, ListItem, ListItemText, ListItemSecondaryAction, IconButton } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+
 const ReadingList =()=>{
     return(
-        <p>reading list</p>
+        <List>
+            <ListItem>
+                <p>The list</p>
+            </ListItem>
+        </List>
+      
     )
 }
 
