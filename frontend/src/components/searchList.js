0efaebbd1ddc2f65130c2940a3de993379f66d6f
Joy Phoebe <joyapisi@gmail.com>
2024-07-01 16:06:00 +0300

    Add a grid list, card and button

--- frontend/src/components/searchList.js
+++ frontend/src/components/searchList.js
@@ -1,6 +1,21 @@
+import { Grid } from "@mui/material";
+import React from "react";
+
+
 const SearchList = () => {
     return(
+        <div>
         <p>search List here</p>
+        <Grid>
+          <Card>
+            <CardContent>
+              <Typography variant="h6">title</Typography>
+              <Typography variant="body2" color="textSecondary">author</Typography>
+              <Button>Add Ello Book</Button>
+            </CardContent>
+          </Card>
+        </Grid>
+    </div>
     )
 }
 
