9e30673fb94ca783c2613571b1d8642166979cd3
Joy Phoebe <joyapisi@gmail.com>
2024-07-01 16:19:31 +0300

    Import apollo's usequery and GET_BOOKS to filter/query books

--- frontend/src/components/searchList.js
+++ frontend/src/components/searchList.js
@@ -1,31 +1,34 @@
+import { useQuery } from "@apollo/client";
 import { Button, Card, CardContent, Grid, Typography } from "@mui/material";
 import React from "react";
+import { GET_BOOKS } from "../queries";
 
+const SearchList = ({searchQuery}) => {
+    const {data} = iseQuery(GET_BOOKS);
+    const filteredBooks = data.books.filter((book) =>
+    book.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
-const SearchList = () => {
-    
-    const filteredBooks = data.books.filter(book =>
-        book.title.toLowerCase().includes(searchQuery.toLowerCase())
-      );
-
-    return(
-        <div>
-        <p>search List here</p>
-        <Grid container>
-        {filteredBooks.map(book => (
-            <Grid item key={book.title} >
-          <Card>
-            <CardContent>
-              <Typography variant="h6">title</Typography>
-              <Typography variant="body2" color="textSecondary">author</Typography>
-              <Button>Add Ello Book</Button>
-            </CardContent>
-          </Card>
+  return (
+    <div>
+      <p>search List here</p>
+      <Grid container>
+        {filteredBooks.map((book) => (
+          <Grid item key={book.title}>
+            <Card>
+              <CardContent>
+                <Typography variant="h6">title</Typography>
+                <Typography variant="body2" color="textSecondary">
+                  author
+                </Typography>
+                <Button>Add Ello Book</Button>
+              </CardContent>
+            </Card>
           </Grid>
         ))}
-        </Grid>
+      </Grid>
     </div>
-    )
-}
+  );
+};
 
 export default SearchList;
